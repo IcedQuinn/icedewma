5e794cc4e5c438afb4b8e20fdbcf340ba95d5b07
Iced Quinn <icedquinn@iceworks.cc>
2022-06-22 00:19:24 -0500

    import source code from aubergine

---
+++ ewma.nim
@@ -0,0 +1,39 @@
+
+type
+   EwmaBase*[T] = object
+      value*, alpha*: T
+
+   Ewma* = EwmaBase[float]
+   EwmaF* = EwmaBase[float32]
+   EwmaD* = EwmaBase[float64]
+
+   EwsdBase*[T] = object
+      mu*: EwmaBase[T]
+      value*: T
+
+   Ewsd* = EwsdBase[float]
+   EwsdF* = EwsdBase[float32]
+   EwsdD* = EwsdBase[float64]
+
+func update*[T](self: var EwmaBase[T]; integral: T) =
+   ## Integrates a new value with the moving average.
+   self.value =
+      (integral.T * self.alpha) +
+      ((1.T - self.alpha) * self.value)
+
+func update*[T](self: var EwsdBase[T]; integral: T) =
+   ## Integrates a new value with the moving average and deviation.
+   let k = (integral - self.mu.value)
+   let k2 = k * k
+   self.value =
+      (1 - self.mu.alpha) *
+      (self.value + (self.mu.alpha * k2))
+   self.mu.update(integral)
+
+func set_n_days*[T](self: var EwmaBase[T]; days: int) =
+   ## Configures the EWMA to average results over `days` number of days while receiving one sample per day.
+   self.alpha = 2.T / (days.T + 1)
+
+func set_n_days*[T](self: var EwsdBase[T]; days: int) =
+   self.mu.set_n_days(days)
+
